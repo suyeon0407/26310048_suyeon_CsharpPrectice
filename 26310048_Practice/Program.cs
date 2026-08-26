4a6a9dc1883e2490e18938a62529f9f7e3e81482
suyeon0407 <kimsouyeon42@gmail.com>
2026-08-26 12:38:54 +0900

    프로그래밍 프로젝트 생성

---
+++ 26310048_Practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _26310048_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
