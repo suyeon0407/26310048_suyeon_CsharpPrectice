a7e917f06619789336d9f2145e6d9ef773a72e6c
suyeon0407 <kimsouyeon42@gmail.com>
2026-08-26 12:57:26 +0900

    이력서 연습

--- 26310048_Practice/Program.cs
+++ 26310048_Practice/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("나의 이력서");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(10, 5);
+            Console.WriteLine("이름");
+            Console.WriteLine("특징");
         }
     }
 }
