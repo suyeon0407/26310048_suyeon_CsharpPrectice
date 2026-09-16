341eac529a802f229cd3efc6203fc57ec7a9805b
suyeon0407 <kimsouyeon42@gmail.com>
2026-09-16 12:21:08 +0900

    조건문, 반복문 연습

---
+++ 26310048_Practice/Loop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Linq;
+
+namespace Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //loop
+            //for
+            //while
+            //do-while
+            //foreach
+
+            //초기 조건 식, 유효성, 스텝
+            //for (int i = 0; i < 100; ++i)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int i = 0;
+            //do
+            //{
+            //    if (!(i < 100))
+            //        break;
+            //    Console.WriteLine(i);
+            //    ++i;
+            //} 
+            //while (false);
+
+            //List<int> myArray = new();
+            //for (int j = 1; j < 100; ++j)
+            //{
+            //    myArray.Add(j);
+            //}
+
+            //int sum = 0;
+            //int i = 0;
+            //while (i < myArray.Count)
+            //{
+            //    sum += myArray[i];
+            //    i++;
+            //}
+            //Console.WriteLine($"1~100 sum :{sum}");
+
+            //한글 문자 출력
+            for(var k = '가'; k<= '힣'; ++k)
+                Console.Write(k);
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ 26310048_Practice/csharpIfCondition/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace csharpIfCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = Console.ReadLine();
+            //int iValue = Int32.Parse(input);
+
+            //int n = 7;
+
+            //if (0 == iValue % n)
+            //{
+            //    Console.WriteLine($"{n}의 배수");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("홀수");
+            //}
+
+            ////Time
+            //Console.WriteLine(DateTime.Now.ToString());
+
+            //Console.WriteLine(DateTime.Now.Year);
+            //Console.WriteLine(DateTime.Now.Month);
+            //Console.WriteLine(DateTime.Now.Day);
+            //Console.WriteLine(DateTime.Now.Date);
+            //Console.WriteLine(DateTime.Now.Hour);
+
+            //if (DateTime.Now.Hour < 12)
+            //{
+            //    Console.WriteLine("오전");
+            //}
+            //else if(DateTime.Now.Hour == 12)
+            //{
+            //     Console.WriteLine("정오");
+            //}
+            //else
+            //{
+            //     Console.WriteLine("오후");
+            //}
+
+            //Console.WriteLine("성적을 입력하세요");
+            //var oopScore = Int32.Parse(Console.ReadLine());
+
+            //if(60 <= oopScore && oopScore < 70)
+            //{
+            //    Console.WriteLine("D");
+            //}
+            //else if (70 <= oopScore && oopScore < 80)
+            //{
+            //    Console.WriteLine("C");
+
+            //}
+            //else if (80 <= oopScore && oopScore < 90)
+            //{
+            //    Console.WriteLine("B");
+
+            //}
+            //else if(90 <= oopScore && oopScore < 100)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if(oopScore >= 60)
+            //{
+            //    Console.WriteLine("D");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("F");
+            //}
+
+            //string input = Console.ReadLine();
+            //int iValue = Int32.Parse(input);
+
+            //switch (iValue % 2)
+            //{
+            //    case 0:
+            //        Console.WriteLine($"2의 배수");
+            //        break;
+
+            //    case 1:
+            //        Console.WriteLine("홀수");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("기타");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("default");
+            //        break;
+            //}
+
+            //string input = Console.ReadLine();
+            //int iValue = Int32.Parse(input);
+
+            //Console.WriteLine(0 == iValue % 2?"2의 배수" : "홀수");
+
+            //var Rank = (90 <= iValue) ? "A" :
+            //           (80 <= iValue) ? "B" :
+            //           (70 <= iValue) ? "C" :
+            //           (60 <= iValue) ? "D" : "F";
+
+            //Console.WriteLine(Rank);
+
+            //string input = Console.ReadLine();
+            //if (input.Contains("Hello"))
+            //{
+            //    Console.WriteLine("Has Hello");
+            //}
+            //else
+            //    Console.WriteLine("Has not Hello");
+
+            //while (true)
+            //{
+            //    Console.WriteLine("키를 입력하세요 :");
+            //    var keyInfo = Console.ReadKey();
+            //    Console.WriteLine('\n');
+            //    Console.WriteLine(keyInfo.Key);
+            //}
+
+            {
+                string? input = Console.ReadLine();
+                int age = Int32.Parse(input);
+
+                if (18 <= age)
+                    Console.WriteLine("성인");
+                else
+                    Console.WriteLine("미성년자");
+            }
+            {
+                string? input = Console.ReadLine();
+                int ivalue = Int32.Parse(input);
+                if (0 == ivalue % 3)
+                {
+                    Console.WriteLine("3의 배수");
+                }
+                else
+                {
+                    Console.WriteLine("3의배수가 아님");
+                }
+            }
+
+
+
+        }
+    }
+}
